b0d7e24b95e825a7618189f4738f265408862106
Tiger Ji <tiger.j540@gmail.com>
2024-05-09 00:17:20 -0400

    feat: add footer component

---
+++ src/app/(components)/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-14 m-24 text-center">
+      <a href="https://github.com/taiga-forestry" target="_blank" className="hover-darken">
+        taiga-forestry ♮ Tiger Ji ♮ tiger_ji@brown.edu
+      </a>
+    </footer>
+  );
+}
