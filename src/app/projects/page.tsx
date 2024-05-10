050667ec406b2b95cb81c29080c5e258e07f5a61
Tiger Ji <tiger.j540@gmail.com>
2024-05-10 01:19:52 -0400

    fix: adjust padding for projects page on mobile screens

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,7 +15,7 @@ export default function ProjectsPage() {
         <SideLine text="Featured Work" />
         <p className="text-14"> A few carefully curated projects that I&apos;m particularly proud of. </p>
 
-        <section className="grid lg:grid-cols-2 xs:p-24 lg:p-36 gap-24 justify-center">
+        <section className="grid lg:grid-cols-2 xs:py-24 md:p-36 gap-24 justify-center">
           <MainProject title="MoWSE" description="Building out a scalable, fully distributed search engine from scratch" src="mowse" tools={["JavaScript", "Bash", "AWS"]} links={{ github: "FIXME", demo: "FIXME" }} />
           <MainProject title="The Critical Review" description="Revamping Brown University's official site for course & professor reviews" src="cr" tools={["Next.js", "MySQL", "Jest"]} links={{ demo: "https://thecriticalreview.org/" }} />
           <MainProject title="Neural Decompiler" description="Demystifying x86-64 assembly with transformer neural networks " src="decompiler" tools={["Python", "TensorFlow", "Flask"]} links={{ github: "https://github.com/taiga-forestry/neural-decompiler", demo: "https://neuraldecompiler.pythonanywhere.com/" }} />
@@ -25,7 +25,7 @@ export default function ProjectsPage() {
         <SideLine text="Other Projects" />
         <p className="text-14"> An assortment of some other notable work that might pique your interest. </p>
 
-        <section className="grid md:grid-cols-2 p-24 justify-center">
+        <section className="grid md:grid-cols-2 xs:py-24 sm:p-24 justify-center">
           <OtherProject title="Partiful" description="Designing new, fun features for Gen Z's favorite party planning site" tools={["Figma"]} links={{ demo: "https://cs1300-iterative-design.vercel.app/" }} />
           <OtherProject title="Word Hunt Solver" description="Solving my favorite word game like a true computer scientist" tools={["Python, Flask"]} links={{ github: "https://github.com/taiga-forestry/Word-Hunt-Solver", demo: "https://whsolver.pythonanywhere.com/" }} />
           <OtherProject title="SmartBills" description="Simplifying shared expense tracking once and for all" tools={["Next.js", "Firebase"]} links={{ github: "FIXME", demo: "FIXME" }} />
